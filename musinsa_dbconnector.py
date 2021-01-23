ee2061cadad314f6962889957d57d7f5f355aefc
이준혁 <j005580@naver.com>
2021-01-23 13:00:01 +0900

    [Fix] 데이터 저장시 Connection이 갱신되는 문제 수정

--- musinsa_dbconnector.py
+++ musinsa_dbconnector.py
@@ -9,6 +9,16 @@ AWS_RDS_PASSWD=os.environ.get("AWS_RDS_PASSWD")
 AWS_RDS_HOST=os.environ.get("AWS_RDS_HOST")
 AWS_RDS_DB=os.environ.get("AWS_RDS_DB")
 
+conn = pymysql.connect(
+    user=AWS_RDS_USER,
+    passwd=AWS_RDS_PASSWD,
+    host=AWS_RDS_HOST,
+    port=int(AWS_RDS_PORT),
+    db=AWS_RDS_DB,
+    charset='utf8'
+    )
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+ 
 price_sql = "INSERT INTO price(product_id, rank, price, del_price, rating, rating_count, created_date, coupon, real_price) values(%s,%s, %s, %s, %s, %s, %s, %s, %s)"
 product_sql = """INSERT INTO product(product_id, img, product_name, product_url, brand, brand_url, modified_date, category, rank, real_price) 
                 values(%s,%s, %s, %s, %s, %s, %s, %s, %s, %s) 
@@ -16,6 +26,7 @@ product_sql = """INSERT INTO product(product_id, img, product_name, product_url,
                 product_id = values(product_id), img = values(img), product_name = values(product_name), product_url = values(product_url), brand = values(brand), 
                 brand_url = values(brand_url), modified_date = values(modified_date) , category = values(category), rank = values(rank), real_price = values(real_price)"""
 pages = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91]
+
 for page in pages:
     data = crawling(page, page+9)
     price_list=[]
@@ -25,15 +36,6 @@ for page in pages:
         product = (str(d["item_id"]), str(d["img"]), str(d["product_name"]), str(d["product_url"]), d["brand"], str(d["brand_url"]), str(d["time"]), d["category"],str(d["rank"]), str(int(d["price"]) + int(d["coupon"])))
         price_list.append(price)
         product_list.append(product)
-    conn = pymysql.connect(
-    user=AWS_RDS_USER,
-    passwd=AWS_RDS_PASSWD,
-    host=AWS_RDS_HOST,
-    port=int(AWS_RDS_PORT),
-    db=AWS_RDS_DB,
-    charset='utf8'
-    )
-    cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.executemany(price_sql, price_list)
     cursor.executemany(product_sql, product_list)
     print("page done!")
